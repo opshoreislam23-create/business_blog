2b1c8be2b7e2310e5c3803e8dedf30c10c8c3afb
Opshore Islam <opshoreislam23@gmail.com>
2025-12-19 23:16:52 +0600

    update blog post

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
 from flask import Flask, render_template
+import os, markdown, datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+BLOG_DIR = "blogs"
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    files = os.listdir(BLOG_DIR)
+    posts = []
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+    for file in files:
+        if file.endswith(".md"):
+            slug = file.replace(".md", "")
+            date = datetime.datetime.fromtimestamp(
+                os.path.getctime(f"{BLOG_DIR}/{file}")
+            ).strftime("%d %b %Y")
 
-if __name__ == '__main__':
+            with open(f"{BLOG_DIR}/{file}", "r", encoding="utf-8") as f:
+                content = f.read()
+                preview = content[:120] + "..."
+
+            posts.append({"slug": slug, "date": date, "preview": preview})
+
+    return render_template("index.html", posts=posts)
+
+@app.route("/blog/<slug>")
+def post(slug):
+    try:
+        with open(f"{BLOG_DIR}/{slug}.md", "r", encoding="utf-8") as f:
+            content = f.read()
+        html = markdown.markdown(content)
+        date = datetime.datetime.fromtimestamp(
+            os.path.getctime(f"{BLOG_DIR}/{slug}.md")
+        ).strftime("%d %b %Y")
+
+        return render_template("post.html", title=slug, content=html, date=date)
+    except:
+        return "Blog not found", 404
+
+if __name__ == "__main__":
     app.run(debug=True)
+
